6cc591ee379b00a0f742a514a32a3da91ee18b8f
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-02-17 18:11:24 +0000

    Added commits and fixed errors.

--- src/client.py
+++ src/client.py
@@ -1,20 +1,19 @@
 import socket
 
-# Host address and port numbers 
+# Host address and port numbers.
 host_ip_address = "127.0.0.1"
-port = 1024
+port = 1025
 
-#creates the socket
+#creates the socket.
 try:
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket was created")
 except socket.error as e:
     print(f"Socket creation encountered error: {e}")
 
+# Attempts to connect to the server program.
 try:
-    socket.connect(host_ip_address, port)
+    socket.connect((host_ip_address, port))
     print(f"Successfully connected on port: {port}.")
-    
-    print(socket.recv(1024).decode)
 except:
     print(f"Error connecting to host on port: {port}.")
--- src/server.py
+++ src/server.py
@@ -1,22 +1,25 @@
 import socket
 
+# Host address and port number.
 host_ip = "127.0.0.1"
-port = 1024
+port = 1025
 
-socket = socket.socket()
+# Socket object to create
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket created successfully.")
 
+# Socket bind to host
 socket.bind((host_ip, port))
 print(f"Socket binded to port: {port}.")
 
+# Socket listening for connections
 socket.listen()
 print("Socket is listening")
 
+# Keeps the server program listening for incoming connections
 while True:
-
+    # Accepts connections to from clients
     client, address = socket.accept()
     print(f"Connected to client at: {address}")
-
-    client.send("You have successfully connected.".encode())
 
     client.close()
