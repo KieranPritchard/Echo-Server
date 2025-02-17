eca8418aa752b3c9c3ca5bd0716bda1a4f44b17c
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-02-17 20:02:20 +0000

    changed socket object names on client.py

--- src/client.py
+++ src/client.py
@@ -6,21 +6,21 @@ port = 1025
 
 #creates the socket.
 try:
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket was created")
 except socket.error as e:
     print(f"Socket creation encountered error: {e}")
 
 # Attempts to connect to the server program.
 try:
-    socket.connect((host_ip_address, port))
+    client_socket.connect((host_ip_address, port))
     print(f"Successfully connected on port: {port}.")
 
     while True:
         echo_input = input("Please input message to send: ")
         
         # Sends message to echo
-        socket.send(echo_input.encode())
+        client_socket.send(echo_input.encode())
 
 except:
     print(f"Error connecting to host on port: {port}.")
