1d7a8c21e75dc839a98c86d0628a871352f5627b
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-02-18 21:23:47 +0000

    Added if statement to check for when user wants to exit.

--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 # Host address and port numbers.
 host_ip_address = "127.0.0.1"
@@ -18,12 +19,15 @@ try:
     print(f"Successfully connected on port: {port}.")
 
     while True:
-        echo_input = input("Please input message to send: ")
-        
-        # Sends message to echo
-        client_socket.send(echo_input.encode())
-        echo_receive = client_socket.recv(1024)
-        print(f"Echo Received: {echo_receive}")
+        echo_input = input("Please input message to send or quit to exit: ").lower()
+
+        if echo_input != "quit":
+            # Sends message to echo
+            client_socket.send(echo_input.encode())
+            echo_receive = client_socket.recv(1024)
+            print(f"Echo Received: {echo_receive}")
+        else:
+            sys.exit()
 
 except:
     print(f"Error connecting to host on port: {port}.")
