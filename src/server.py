9dff4e5909b15cfda18855dbf66919d4588e1720
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-02-22 21:52:15 +0000

    fixed the code and made for allowing it to receive multiple messages.

--- src/server.py
+++ src/server.py
@@ -13,12 +13,27 @@ print(f"Server started, listening on {host_ip}:{port}")
 # Keeps the server program listening for incoming connections
 while True:
     # Accepts connections to from client
-    client, address = socket.accept()
+    client, address = server_socket.accept()
     print(f"Connected to client at: {address}")
 
-    # Receives input from client
-    echo_receive = client.recv(1024).decode()
-    print(f"Received from client: {echo_receive}")
+    # Handles multiple input from client
+    while True:
+        try:
+            echo_receive = client.recv(1024)
+
+            # Detects client disconnect
+            if not echo_receive:
+                print(f"Client at {address} disconnected")
+                break
+
+            message = echo_receive.decode()
+            print(f"Message received from client: {message}")
+
+            # Sends the echo response
+            client.send(message.encode())
+        except socket.error as e:
+            print(f"Error encountered {e}")
+    client.close()
 
     # Sends client echo back
     client.send(echo_receive.encode())
